5d1d66745763090465b18711b5e8d2c0cd7aa0f8
axelheng <axelheng04@gmail.com>
2026-08-25 15:37:39 +0800

    Complete JavaFX tutorial part 2

---
+++ src/main/java/DialogBox.java
@@ -0,0 +1,20 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogBox extends HBox {
+    private final Label text;
+    private final ImageView displayPicture;
+
+    public DialogBox(String s, Image i) {
+        text = new Label(s);
+        displayPicture = new ImageView(i);
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        setAlignment(Pos.TOP_RIGHT);
+        getChildren().addAll(text, displayPicture);
+    }
+}
